96086d6afae952fc4e8ab65d57ee3b1bae9579f2
Nikolai <nikolaigauer@gmail.com>
2017-06-30 20:54:21 +0000

    Added comments explaing how my code works

--- moduleImport.js
+++ moduleImport.js
@@ -1,3 +1,6 @@
+// declaring a variable `moduleImport`
+// which requires the module containing
+//the function I want to invoke
 var moduleImport = require("./myModule");
 
 // invokes the function `private`
--- myModule.js
+++ myModule.js
@@ -1,8 +1,11 @@
-//
+// declaring a private function that can not be invoked outside of this module
 var privateFunction = function() {
   console.log("This is a private function")
 }
+
+// stuffing a function into the exports object, which can be invoked if required
 module.exports = {
+  //the function below invokes the private function called `privateFunction`
   private: function() {
     return privateFunction();
   }
